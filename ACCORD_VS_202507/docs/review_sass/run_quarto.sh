ff4077a9096bd0bec688a996731516e692025bfb
Carlos Peralta <carlos9917@gmail.com>
2025-06-18 11:25:50 +0200

    Added short presentation describing Bent Sass SLX score

---
+++ ACCORD_VS_202507/docs/review_sass/run_quarto.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+NBOOK=carra_quarto_example_west.qmd
+NBOOK=carra_quarto_example_means.qmd
+NBOOK=$1
+#create the html
+quarto preview $NBOOK --no-browser --no-watch-inputs
+
+#can also use
+#quarto render $NBOOK 
+
+
+#convert to ipynb
+#quarto convert $NBOOK
